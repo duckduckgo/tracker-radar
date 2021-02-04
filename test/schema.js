ed1d4a2a7da36bfdf8475e3739456b6d3d9acc02
Jason Dorweiller <jason@duckduckgo.com>
2021-02-04 10:37:48 -0500

    add tests

---
+++ test/schema.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "domain": {
+        "type": "object",
+        "properties": {
+            "domain": {"type": "string"},
+            "owner": {"type": "object"},
+            "prevalence": {"type": "number"},
+            "fingerprinting": {"type": "integer", "minimum": 0, "maximum": 3},
+            "cookies": {"type": "number"},
+            "categories": {"type": "array"},
+            "default": {"type": "string"},
+            "rules": {"type": "array"}
+        }
+    },
+    "rule": {
+        "type": "object",
+        "properties": {
+            "rule": {"type": "string"},
+            "fingerprinting": {"type": "integer", "minimum": 0, "maximum": 3},
+            "cookies": {"type": "number"}
+        }
+    },
+    "entity": {
+        "type": "object",
+        "properties": {
+            "properties": {"type": "array"},
+            "displayName": {"type": "string"},
+            "name": {"type": "string"},
+            "prevalence": {
+                "type": "object",
+                "properties": {
+                    "tracking": {"type": "number"}
+                }
+            }
+        },
+        "required": ["name", "displayName", "properties"]
+    }
+}
---
+++ test/test-schema.js
@@ -0,0 +1,27 @@
+const assert = require('assert')
+const fs = require('fs')
+const Ajv = require('ajv').default
+const ajv = new Ajv()
+const schema = require('./../test/schema.js')
+
+describe('Domain schema', () => {
+    it('should have the correct schema', () => {
+        const regions = fs.readdirSync('domains')
+        for (const region of regions) {
+            for (const file of fs.readdirSync(`domains/${region}`)) {
+                const data = JSON.parse(fs.readFileSync(`domains/${region}/${file}`, 'utf8'))
+                assert.equal(ajv.validate(schema.domain, data), true, `${region}/${file}`)
+            }
+        }
+    })
+})
+
+describe('Entity schema', () => {
+    it('Entities should have the correct schema', () => {
+        const entities = fs.readdirSync('entities')
+        for (const file of entities) {
+            const data = JSON.parse(fs.readFileSync(`entities/${file}`, 'utf8'))
+            assert.equal(ajv.validate(schema.entity, data), true, `${file}`)
+        }
+    })
+})
